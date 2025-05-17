1906e7c5143367f9bf8b2b34d273651a6e54d26b
Whykay243 <yinka.ajibola0011@gmail.com>
2025-05-17 19:49:27 -0400

    updated files

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -38,8 +38,3 @@ resource "aws_s3_bucket_versioning" "tf_state_bucket_versioning" {
     status = "Enabled"
   }
 }
-
-output "bucket_name" {
-  value       = aws_s3_bucket.tf_state_bucket.bucket
-  description = "Name of the S3 bucket used for Terraform backend"
-}
