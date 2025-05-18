9a0c9246137c2e055e3b3a938c31986357e98604
Whykay243 <yinka.ajibola0011@gmail.com>
2025-05-17 22:17:31 -0400

    Update main.tf

--- infra/modules/ec2/main.tf
+++ infra/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = var.key_name
+  key_name               = var.ec2_key_name   # This should be "physistutors"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_groups
 
